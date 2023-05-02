e9eb0df000ad6b2b9e75976e8c9fbbfab5757967
Adrian Ortiz Velez <aortizsax@gmail.com>
2023-05-01 18:49:52 -0700

    Randomization implementation first complete

--- src/nyemtaay/calculate/informationtheory.py
+++ src/nyemtaay/calculate/informationtheory.py
@@ -28,7 +28,7 @@
 ## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ##
 ##############################################################################
-#
+
 
 # Libraries
 import numpy as np
@@ -40,44 +40,30 @@ import os
 import math
 
 
-def sequences_to_gamete_prob(sequence_dataframe, data, identifier,geo):
-    print(sequence_dataframe)
-    print(data)
-    print(identifier)
+def sequences_to_gamete_prob(sequence_dataframe, data, ID,geo):
     
     #convert to gamete probablity dictionary per deme
-    
-    
-    # identififer for subpopulaitons
-    ID = identifier
-
-    # initialize denom and numerator
-    H_s_sub = {}
-    H_i_sub = {}
 
     # identify subpopulations
     subpopulations = data[ID].unique()
 
-    # H_s
     gamete_dict = {}
     population_dict = {}
     for subpopulation in subpopulations:
-        H_s_sub[subpopulation] = []  # positional array
         isSUBPOPULATION = data[ID] == subpopulation
         population_dict[subpopulation] = isSUBPOPULATION.sum()
         subpopulation_sequences = sequence_dataframe[isSUBPOPULATION]
         gamete_dict[subpopulation] = pd.Series(subpopulation_sequences.values.tolist()).map(lambda x: ''.join(map(str,x)))
 
     gamete_df = pd.DataFrame.from_dict(gamete_dict)
+    gamete_df#randomized combinations of this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     gamete_counts = gamete_df.apply(pd.Series.value_counts).fillna(0)
     #gamete_sums
     gamete_probabilty = gamete_counts.div(isSUBPOPULATION.sum(),axis=1)
-    print(gamete_probabilty)
     
-    return (gamete_probabilty, population_dict)
+    return (gamete_probabilty, population_dict)#add random gamete sequences??
     
-    
-    
+
 def demes_shannon_entropy(gamete_probabilty):  
     print("shannon_entropy",gamete_probabilty.apply(shannon_entropy,axis=0))
     return gamete_probabilty.apply(shannon_entropy,axis=0)    
@@ -92,7 +78,6 @@ def shannon_entropy(row_p):
         H_sum = p_i * np.log(p_i)
 
     H *= H_sum
-    
     return H
     
 def demes_jsd(gamete_probabilty, population_dict):
@@ -102,32 +87,28 @@ def demes_jsd(gamete_probabilty, population_dict):
     jsd_dict = {}
     D_dict = {}
     I_dict = {}
+    I_R_dict_list = {}
+    I_R_dict_mu_std = {}
     for (deme_p, row_p) in gamete_probabilty.items():
         n_p = population_dict[deme_p]
         for (deme_q, row_q) in gamete_probabilty.items():
             n_q = population_dict[deme_q]
             if deme_p != deme_q:
                 comparison = deme_p + '->' + deme_q
-                print(comparison)
                 (w_p, w_q) = statisical_weights(n_p, n_q)
                 jsd_dict[comparison] = jsd(row_p, row_q, w_p, w_q)
-                D_dict[comparison] = jsd_normalized_to_max(row_p, row_q, w_p, w_q)
                 
-                X = union(row_p, row_q)
-                J = intersection(row_p, row_q)
-                print("X",X,"J",J)
-                D = disjoint(X,J)
-                print("D",D)
-                I = information_flow_directionality(row_p,row_q,X,J)
-                I_dict[comparison] = I 
-                
+        
                 
     print("JSD",jsd_dict)
-    print("D",D_dict)
-    return None
 
-def jsd(row_p, row_q, w_p, w_q):
+    return jsd_dict
     
+    
+def jsd(row_p, row_q, w_p, w_q):
+    """
+    JSD(P||Q) = ...
+    """
     jsdiv = 0
     
     jsdiv += shannon_entropy(w_p*row_p + w_q*row_q)
@@ -165,6 +146,40 @@ def statisical_weights(n_1,n_2):
         
     return (w_1, w_2)
 
+def demes_norm_jsd(gamete_probabilty, population_dict):
+    """
+    JSD(P||Q) = ...
+    """
+    jsd_dict = {}
+    D_dict = {}
+    I_dict = {}
+    I_R_dict_list = {}
+    I_R_dict_mu_std = {}
+    for (deme_p, row_p) in gamete_probabilty.items():
+        n_p = population_dict[deme_p]
+        for (deme_q, row_q) in gamete_probabilty.items():
+            n_q = population_dict[deme_q]
+            if deme_p != deme_q:
+                comparison = deme_p + '->' + deme_q
+                (w_p, w_q) = statisical_weights(n_p, n_q)
+                jsd_dict[comparison] = jsd(row_p, row_q, w_p, w_q)
+                D_dict[comparison] = jsd_normalized_to_max(row_p, row_q, w_p, w_q)
+                
+                X = union(row_p, row_q)
+                J = intersection(row_p, row_q)
+                D = disjoint(X,J)
+                I = information_flow_directionality(row_p,row_q,X,J)
+                I_dict[comparison] = I 
+                
+                #shuffle population sequences 1000 times 
+                #get norm mean and std
+                
+                
+    print("D",D_dict)
+
+    return D_dict
+
+
 def jsd_normalized_to_max(row_p, row_q, w_p, w_q):
 
     D = jsd(row_p, row_q, w_p, w_q)
@@ -172,9 +187,41 @@ def jsd_normalized_to_max(row_p, row_q, w_p, w_q):
     demonenator += -w_q * np.log(w_q)
     D /= demonenator
     return D
+    
+    
+def demes_info_flow_direction(gamete_probabilty, population_dict):
+    """
+    I(P||Q) = ...
+    """
+    I_dict = {}
+    I_R_dict_list = {}
+    I_R_dict_mu_std = {}
+    for (deme_p, row_p) in gamete_probabilty.items():
+        n_p = population_dict[deme_p]
+        for (deme_q, row_q) in gamete_probabilty.items():
+            n_q = population_dict[deme_q]
+            if deme_p != deme_q:
+                comparison = deme_p + '->' + deme_q
+                (w_p, w_q) = statisical_weights(n_p, n_q)
+                
+                
+                
+                X = union(row_p, row_q)
+                J = intersection(row_p, row_q)
+                D = disjoint(X,J)
+                I = information_flow_directionality(row_p,row_q,X,J)
+                I_dict[comparison] = I 
+                
+                #shuffle population sequences 1000 times 
+                #get norm mean and std
+                
+
+    print("I",I_dict)
+    return I_dict
 
 def information_flow_directionality(row_p,row_q,X,J):
-
+    """
+    """
     (mu_p, mu_q, mu_pq) = index_mu_PQ(row_p,row_q,X,J)
 
     P_bar_J = row_p[J]/row_p[J].sum()
@@ -184,12 +231,12 @@ def information_flow_directionality(row_p,row_q,X,J):
     weighted_subset_H_q = shannon_entropy(Q_bar_J) / mu_q
 
     I = np.sign(weighted_subset_H_q - weighted_subset_H_p)
-    print("I",I>0,I)
-    return None
+    return I
 
 
 def index_mu_PQ(row_p,row_q,X,J):
-    
+    """
+    """
     mu_p = 0
     mu_q = 0
     mu_pq = 0
@@ -211,6 +258,115 @@ def index_mu_PQ(row_p,row_q,X,J):
     mu_p /= demon_p
     mu_q /= demon_q
     mu_pq /= demon_pq
-    
     return (mu_p, mu_q, mu_pq)
+
+
+
+def sequences_to_random_deme_combinations(sequence_dataframe, data, identifier,geo):
+    
+    #convert to gamete probablity dictionary per deme
+    rng = np.random.default_rng(12345)
+    
+    # identififer for subpopulaitons
+    ID = identifier
+
+    # initialize denom and numerator
+    H_s_sub = {}
+    H_i_sub = {}
+
+    # identify subpopulations
+    subpopulations = data[ID].unique()
+
+    # H_s
+    gamete_dict = {}
+    population_dict = {}
+    for subpopulation in subpopulations:
+        H_s_sub[subpopulation] = []  # positional array
+        isSUBPOPULATION = data[ID] == subpopulation
+        population_dict[subpopulation] = isSUBPOPULATION.sum()
+        subpopulation_sequences = sequence_dataframe[isSUBPOPULATION]
+        gamete_dict[subpopulation] = pd.Series(subpopulation_sequences.values.tolist()).map(lambda x: ''.join(map(str,x)))
+
+    gamete_df = pd.DataFrame.from_dict(gamete_dict)
+    gamete_df#randomized combinations of this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    
+    gamete_random_combinations_dict = {}
+    for (deme_p,gamete_sequences) in gamete_df.items():
+
+        for (deme_q, row_q) in gamete_df.items():
+            if deme_p != deme_q:
+                comparison = deme_p + '->' + deme_q
+                gamete_random_combinations_dict[comparison] = []
+
+                for i in range(100):
+                    comparison_sequences = gamete_df[[deme_p,deme_q]].copy(deep=True)
+                    #.sample(frac=1,random_state=1)
+                    
+                    
+                    dn=[]
+                    for deme in [deme_p,deme_q]:
+                        list_of_random_sequences = []
+                        
+                        col = rng.integers(low=0, high=2, size=population_dict[deme_p])
+                        row = rng.integers(low=0, high=population_dict[deme_p], size=population_dict[deme_p])
+                        
+                        for element_index, random_index in enumerate(col):
+                            random_jndex = row[element_index]
+                            list_of_random_sequences.append(comparison_sequences.iloc[:,random_index][random_jndex])
+
+                        df1 = pd.DataFrame({'sequence': range(population_dict[deme]),
+                                     deme: list_of_random_sequences,
+                                    }).set_index('sequence')
+                        dn.append(df1)
+                            
+                    comparison_random_sequences = pd.concat(dn, axis=1)
+                    
+                    rand_gamete_counts = comparison_random_sequences.apply(pd.Series.value_counts).fillna(0)
+                    #gamete_sums
+                    rand_gamete_probabilty = rand_gamete_counts.div(isSUBPOPULATION.sum(),axis=1)                
+
+                    gamete_random_combinations_dict[comparison].append(rand_gamete_probabilty)
+    
+    
+    return (gamete_random_combinations_dict, population_dict)#add random gamete sequences??
+
+def randomized_information_flow_directionality(gamete_random_combinations_dict,I_dict):
+    I_R_array = []
+    
+    IR_dict ={}
+    R_dict ={}
+    
+    for comparison, list_of_gamete_probabilites in gamete_random_combinations_dict.items(): 
+        deme_p = comparison[0]
+        deme_q = comparison[-1]
+        I_pq = I_dict[comparison]
+        
+        
+        for i in range(100):
+            gamete_probabilty = list_of_gamete_probabilites[i] 
+            row_p = gamete_probabilty[deme_p]
+            row_q = gamete_probabilty[deme_q]
+            
+            X = union(row_p, row_q) 
+            J = intersection(row_p, row_q) 
+            D = disjoint(X,J) 
+            I = information_flow_directionality(row_p,row_q,X,J) 
+            I_R_array.append(I)
+
+
+        std_I_R = np.std(I_R_array, dtype=np.float64)
+        mu_I_R = np.mean(I_R_array, dtype=np.float64)
+        IR_dict[comparison] = (mu_I_R, std_I_R)
+        
+        R = abs(I_pq - mu_I_R)
+        R /= std_I_R
+        
+        R_dict[comparison] = R
+        
+    print("IR ",IR_dict)
+    print("R ",R_dict)    
+    
+        
+    return IR_dict
 
